64999be2dad37a408a8e837a2f2031f6eaec72fa
Neon-Panda <174306477+Neon-Panda@users.noreply.github.com>
2025-01-27 07:03:40 -0600

    add delete and backspace to calc

--- scripts.js
+++ scripts.js
@@ -3,19 +3,20 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-let num1
-let num2
-let operator
+let num1 = ""
+let num2 = ""
+let operator = ""
+let sum = ""
 
-const operate = (num1, operator, num2) => {
+const operate = (num2, operator, num1) => {
     if (operator === "+") {
-        add(num1, num2)
+        return add(num1, num2)
     } else if (operator === "-") {
-        subtract(num1, num2)
+        return subtract(num1, num2)
     } else if (operator === "*") {
-        multiply(num1, num2)
-    } else if (operater === "/") {
-        divide(num1, num2)
+        return multiply(num1, num2)
+    } else if (operator === "/") {
+        return divide(num1, num2)
     }
 }
 
@@ -24,15 +25,37 @@ let display = document.querySelector(".display");
 let container = document.querySelectorAll("button");
 container.forEach((button) => {
     button.addEventListener("click", () => {
-        if (num1 === undefined && button.className === "operand") {
-            num1 = button.textContent
+        if (button.className === "operand" && num1.length < 9) {
+            num1 += button.textContent
             display.textContent = num1
-        } else if (operator === undefined && button.className == "operator") {
+        } else if (button.className == "operator" || button.className == "equals") {
+            if (num2) {
+                sum = operate(num1, operator, num2)
+                num1 = sum
+                display.textContent = sum
+            } else {
             operator = button.textContent
             display.textContent = operator
-        } else if (num2 === undefined && button.className === "operand") {
-            num2 = button.textContent
-            display.textContent = num2
+            }
+            num2 = num1
+            num1 = ""
+        } else if (button.className === "clear") {
+            num1 = "", num2 = "", operator = "", sum = "";
+            display.textContent = ""
+        } else if (button.className === "backspace") {
+            let array
+            if (sum) {
+                sumString = sum.toString() 
+                array = sumString.split("");
+                array.pop()
+                sum = array.join("")
+                display.textContent = sum;
+            } else {
+                array = num1.split("");
+                array.pop()
+                num1 = array.join("")
+                display.textContent = num1;
+            }
         }
     })
 })
