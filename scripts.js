a3f1d27a66d047747fc7e07c02fc1edb5c647e4f
Neon-Panda <174306477+Neon-Panda@users.noreply.github.com>
2025-01-27 05:00:08 -0600

    add functions to calc, fix html .js link

--- scripts.js
+++ scripts.js
@@ -1,1 +1,38 @@
-t
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+let num1
+let num2
+let operator
+
+const operate = (num1, operator, num2) => {
+    if (operator === "+") {
+        add(num1, num2)
+    } else if (operator === "-") {
+        subtract(num1, num2)
+    } else if (operator === "*") {
+        multiply(num1, num2)
+    } else if (operater === "/") {
+        divide(num1, num2)
+    }
+}
+
+let display = document.querySelector(".display");
+
+let container = document.querySelectorAll("button");
+container.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (num1 === undefined && button.className === "operand") {
+            num1 = button.textContent
+            display.textContent = num1
+        } else if (operator === undefined && button.className == "operator") {
+            operator = button.textContent
+            display.textContent = operator
+        } else if (num2 === undefined && button.className === "operand") {
+            num2 = button.textContent
+            display.textContent = num2
+        }
+    })
+})
